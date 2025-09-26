de08a957ba0242fce6a0836bf9aa37b007630b4a
tamanna5678 <157673601+tamanna5678@users.noreply.github.com>
2025-09-26 14:18:47 +0530

    Update url.js

--- models/url.js
+++ models/url.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const urlSchema = new Schema({
   shortId: String,
   originalUrl: String,
 });
-
 module.exports = mongoose.model("Url", urlSchema);
